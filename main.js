733bf35782a79b01854efef803d9b7b1b287aff6
agusssrs <agustinmancini2@hotmail.com>
2023-03-30 15:31:27 -0300

    practica

---
+++ main.js
@@ -0,0 +1,11 @@
+class ropa {
+    constructor (marca, talle){
+        this.marca = marca;
+        this.talle = talle;
+    }
+}
+
+let remera = new ropa ('Nike', 'L');
+let remera2 = new ropa ('Adidas', 'M');
+let short = new ropa ('Jordan', 'L');
+let short2 = new ropa ('Adidas', 'M');
